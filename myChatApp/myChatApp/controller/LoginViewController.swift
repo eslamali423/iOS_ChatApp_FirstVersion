ed0d745309d716e3feebddb5d6df59a444c515bd
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-07 20:17:31 +0200

    create Login View Controller

---
+++ myChatApp/myChatApp/controller/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  myChatApp
+//
+//  Created by Eslam Ali  on 07/03/2022.
+//
+
+import UIKit
+
+
+
+
+class LoginViewController: UIViewController {
+    //MARK:- outlets
+
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func registrationButton(_ sender: Any) {
+    }
+    @IBAction func loginButton(_ sender: Any) {
+    }
+}
